27b8051be9c929a7d2a03024cdd2f07795a73011
marcelogomesandrade <gomesgomes123321@gmail.com>
2023-10-05 15:03:27 -0300

    up

---
+++ js/01-sintaxe.js
@@ -0,0 +1,34 @@
+console.log("programador Web");
+console.log('Senac Penha')
+
+/* Comentários - ALT SHIFT A */
+//Comentários - CRTL ;
+
+
+/* Manipulando dados */
+
+// variáveis 
+let curso = "HTML5 e CSS3"
+let ano = 2023;
+
+// Constantes 
+const mediaMinima = 7
+
+// Saída de dados a partir da memória
+console.log (curso);
+console.log (mediaMinima);
+console.log (ano);
+
+// Saída de dados personalizada 
+
+/* Estou fazendo o curso de HTML5 e CSS3 neste ano de 2023.*/
+
+// Template String/Literal : saída usando crases e $ {}
+
+console.log (`Estou fazendo o curso de ${curso} neste ano de ${ano}.`);
+
+
+// Concatenação (forma mais antiga de saída, mas ainda válida)
+console.log ( "Estou fazendo o curso de "+curso+" neste ano de "+ano+".");
+
+
