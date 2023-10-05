b3908bb15ef5c9f4035bf06ac58940220d803152
marcelogomesandrade <gomesgomes123321@gmail.com>
2023-10-05 15:29:20 -0300

    up

---
+++ js/02-estrutura.js
@@ -0,0 +1,9 @@
+/* Estruturas de Dados
+
+- Arrays (Vetores/Matrizes): Lista de dados indexados, sequenciais e acessíveis por um índice numérico.  
+
+- Objetos : Lista de dados não indexado e formados por propriedades e valores */
+
+// Arrays 
+
+let cursos = ["Node.js","React","SQL","UX/UI Design"];
