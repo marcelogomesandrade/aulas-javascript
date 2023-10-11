cc605b76763598df4c9035869504d46d443fc950
marcelogomesandrade <gomesgomes123321@gmail.com>
2023-10-11 16:03:23 -0300

    y

---
+++ js/05-loops.js
@@ -0,0 +1,23 @@
+// Comandos de Repetição 
+
+//Instruções para realizar ações para uma quantidade especifica de vezes. 
+
+// Comandos tradicionais : 
+
+// - while -> ENQUANTO 
+
+// - do/while -> FAÇA/ENQUANTO
+
+// - for -> PARA 
+
+
+/* OBS.: normalmente o loop é controlado através de uma variável contadora.*/
+
+let contador = 1;
+while (contador <=5 ){ 
+    console.log (`valor de contador: ${contador}`);
+    contador++;
+
+}
+
+
