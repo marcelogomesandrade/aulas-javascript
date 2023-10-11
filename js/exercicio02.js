a38dc77eca9d9ffbc3a44b8e53fd69c102c0b407
marcelogomesandrade <gomesgomes123321@gmail.com>
2023-10-11 17:23:44 -0300

    u

---
+++ js/exercicio02.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let alunos = ["Joãozinho","Mariazinha"," Giuseppezinho"];
+
+let Mariazinha {
+
+    idade=34;
+}
+
+let Joãozinho {
+
+    idade=35;
+
+}
+
+let Giuseppezinho {
+
+    idade=33;
+}
+
