e6147a55ebf731b4fc486209483c599b602bb9fe
marcelogomesandrade <gomesgomes123321@gmail.com>
2023-10-17 14:08:45 -0300

    ip

--- js/exercicio02.js
+++ js/exercicio02.js
@@ -1,9 +1,38 @@
 "use strict";
 
-let alunos = ["Joãozinho","Mariazinha","Giuseppezinho"];
+/*  */
 
-let quantidade = alunos.length;
+ 
 
-for (let i = 0; i < quantidade; i++) {
-    console.log(`Aluno: ${alunos[i]}`);}
+const alunos = [
+
+{id: 1,
+nome: "Joazinho" ,},
+
+{id: 2,
+nome: "mariazinha" ,} ,
+    
+{id: 3,
+nome: "Giuseppezinho" ,}
+]
+
+
+/*   */
+
+
+
+let quantidadeAlunos = alunos.length;
+
+for (let i = 0; i < quantidadeAlunos; i++) {
+    console.log(`Aluno: ${alunos[i].nome}`);}
+
+
+  console.log (`---------`)  
+
+  let i =0;
+  while (i < quantidadeAlunos){
+    console.log(alunos[i].nome);
+    i++;
+
+  }
 
