5e77a6f180cc6b40f7fa63dd9725a8855a721b50
marcelogomesandrade <gomesgomes123321@gmail.com>
2023-10-18 14:56:54 -0300

    up

--- js/exercicio03.js
+++ js/exercicio03.js
@@ -1,22 +1,31 @@
 "use strict"
 
 let valorEmDolares = 1000;
-let valorCotacao =  5.03;
+let valorCotacao = 5.03;
 
-function converterMoeda (valor, cotação){
+function converterMoeda(valor, cotação) {
     return valor * cotação;
 }
 ;
-let valorConvertido = converterMoeda(valorCotacao,valorEmDolares);
+let valorConvertido = converterMoeda(valorCotacao, valorEmDolares);
 
 
 
-console.log (valorEmDolares);
-console.log (valorCotacao);
-    
+console.log(formatarMoeda(valorEmDolares, "en","USD"));
+console.log(formatarMoeda(valorConvertido, "pt-br","BRL"));
 
 
- 
+function formatarMoeda(valor,local,moeda) {
+    let opcoes = {
+        style: "currency",
+            currency: moeda
+
+
+};
+
+    return valor.toLocaleString(local, opcoes)  
+
+}
 
 
 
@@ -28,4 +37,4 @@ console.log (valorCotacao);
 
 
 
- 
+
